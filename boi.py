dfab8d821e2271666337e572a49ebcb4b6b84441
Tristo <15713784+TristoKrempita@users.noreply.github.com>
2018-06-12 12:55:38 +0200

    Added a variable price into object to be displayed
    Trying to still make the cijena variable work so the list of objects can be sorted correctly

--- boi.py
+++ boi.py
@@ -7,14 +7,19 @@ soup = BeautifulSoup(r_html,"html.parser")
 chr = soup.find_all('h3',class_="s-item__title")
 cijene = soup.find_all('span',class_="s-item__price")
 links = soup.find_all('a',class_="s-item__link")
+br=0
 #for c in cijene:
 #    print(str(c.string)[1:])
 
 filterCijena = float(input("Upper limit filter price: "))
 
+for cijen in cijene:
+    print(cijen.text)
+
 class grafa:
     ime = None
     cijena = None
+    cijenaString = None
     link = None
 
 grafe = []
@@ -24,12 +29,15 @@ for graf in range(len(cijene)):
 for i in range(0,len(cijene)):
     #assigns title text to the ime variable of object
     grafe[i].ime = chr[i].text
+    grafe[i].cijenaString = cijene[i].text
     #turning the string we got from the price in the $3.45 format into a 3.45 float
     if(any(char.isdigit() for char in str(cijene[i].string))):
         grafe[i].cijena = float(str(cijene[i].string)[1:])
     #if the price isn't a price at all we set the value to -0.5 and filter it later
     else:
-        grafe[i].cijena = -0.5
+        #TODO skuzit kako uzet prvu znameknu tj prvi clan stringa i pretvorit u float (to je za sortiranje ali display ce bit cijenaString)
+        print("HAH",float(str(cijene[i].text)[2]))
+        grafe[i].cijena = float(str(cijene[i].text)[2])
     #we assign the href memeber of the tag to the link variable of object
     grafe[i].link = links[i]['href']
 
@@ -38,9 +46,11 @@ grafe.sort(key=lambda x: x.cijena, reverse=False)
 #prints list of items until upper limit
 for x in grafe:
     if(x.cijena != None and x.cijena<filterCijena and x.cijena != -0.5):
-        print(x.ime," | ",x.cijena," | ",x.link,"\n")
+        br+=1
+        print(x.ime," | ",x.cijenaString," | ",x.link,"\n")
 
 with open('graficke','w',encoding='utf-8') as file:
     for x in grafe:
         if(x.cijena != None and x.cijena<filterCijena and x.cijena != -0.5):
-            file.write(str(x.ime)+" | "+str(x.cijena)+" | "+str(x.link)+"\n")
+            file.write(str(x.ime)+" | "+str(x.cijenaString)+" | "+str(x.link)+"\n")
+print("Number of items: "+str(br))
