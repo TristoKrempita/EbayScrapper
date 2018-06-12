71d52dc81a5f0dbfd5e3094fe23de0514e39b936
Tristo <15713784+TristoKrempita@users.noreply.github.com>
2018-06-12 14:30:42 +0200

    Fixed the price and variable naming
    Class is now called _item_ and the list of objects is now _items_  
    Price that was in a "$amount to $amount" format is not correctly displayed and part of the sort process (no item on the page is lost)

--- boi.py
+++ boi.py
@@ -16,41 +16,41 @@ filterCijena = float(input("Upper limit filter price: "))
 for cijen in cijene:
     print(cijen.text)
 
-class grafa:
+class item:
     ime = None
     cijena = None
     cijenaString = None
     link = None
 
-grafe = []
-grafeFinal=[]
+items = []
+itemsFinal=[]
 for graf in range(len(cijene)):
-    grafe.append(grafa())
+    items.append(item())
 for i in range(0,len(cijene)):
     #assigns title text to the ime variable of object
-    grafe[i].ime = chr[i].text
-    grafe[i].cijenaString = cijene[i].text
+    items[i].ime = chr[i].text
+    items[i].cijenaString = cijene[i].text
     #turning the string we got from the price in the $3.45 format into a 3.45 float
     if(any(char.isdigit() for char in str(cijene[i].string))):
-        grafe[i].cijena = float(str(cijene[i].string)[1:])
+        items[i].cijena = float(str(cijene[i].string)[1:])
     #if the price isn't a price at all we set the value to -0.5 and filter it later
     else:
         #TODO skuzit kako uzet prvu znameknu tj prvi clan stringa i pretvorit u float (to je za sortiranje ali display ce bit cijenaString)
-        print("HAH",float(str(cijene[i].text)[2]))
-        grafe[i].cijena = float(str(cijene[i].text)[2])
+        print("HAH",float(str(cijene[i].text)[1]))
+        items[i].cijena = float(str(cijene[i].text)[1])
     #we assign the href memeber of the tag to the link variable of object
-    grafe[i].link = links[i]['href']
+    items[i].link = links[i]['href']
 
 #sorting the array of objects by attribute cijena
-grafe.sort(key=lambda x: x.cijena, reverse=False)
+items.sort(key=lambda x: x.cijena, reverse=False)
 #prints list of items until upper limit
-for x in grafe:
+for x in items:
     if(x.cijena != None and x.cijena<filterCijena and x.cijena != -0.5):
         br+=1
         print(x.ime," | ",x.cijenaString," | ",x.link,"\n")
 
-with open('graficke','w',encoding='utf-8') as file:
-    for x in grafe:
+with open('ebayList','w',encoding='utf-8') as file:
+    for x in items:
         if(x.cijena != None and x.cijena<filterCijena and x.cijena != -0.5):
             file.write(str(x.ime)+" | "+str(x.cijenaString)+" | "+str(x.link)+"\n")
 print("Number of items: "+str(br))
