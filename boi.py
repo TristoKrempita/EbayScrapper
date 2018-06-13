ebdd3b78342f757bfa92c9f200be2570b9602be1
Tristo <15713784+TristoKrempita@users.noreply.github.com>
2018-06-13 11:01:01 +0200

    Fixed the dual bid and New Listing bug
    Removed "New Listing" from names of PC's with the "NEWLISTING" tag
    Fixed the bug where 2 prices on an item with a bid or otherwise would show up as different prices for the next item in the list and so on until the whole list is trashed

--- boi.py
+++ boi.py
@@ -13,14 +13,17 @@ soup = BeautifulSoup(r_html,"html.parser")
 chr = soup.find_all('h3',class_="s-item__title")
 cijene = soup.find_all('span',class_="s-item__price")
 links = soup.find_all('a',class_="s-item__link")
-values = [len(chr),len(cijene),len(links)]
-print(values)
-print(cijene[:2])
-print(chr[:2])
 br=0
-#for c in cijene:
-#    print(str(c.string)[1:])
-
+cijeneFinal = []
+for i in range(0,len(cijene)):
+    if((cijene[i].parent.parent.parent.parent.parent == cijene[i-1].parent.parent.parent.parent.parent)):
+        print(cijene[i].text)
+        print(cijene[i-1].text)
+        continue
+    else:
+        cijeneFinal.append(cijene[i])
+values = [len(chr),len(cijeneFinal),len(links)]
+print(values)
 filterCijena = float(input("Upper limit filter price: "))
 print(">"+str(filterCijena))
 class item:
@@ -32,33 +35,35 @@ class item:
 replaced = ""
 items = []
 itemsFinal=[]
+#return this after you fix the bid 2 prices on 1 item being treated as 2 prices on 2 items
 for graf in range(min(values)):
     items.append(item())
-for i in range(0,len(chr)):
+for i in range(0,min(values)):
     #assigns title text to the ime variable of object
-    items[i].ime = chr[i].text
-    items[i].cijenaString = cijene[i].text
+    if(chr[i].text[0:11].lower() == "new listing"):
+        items[i].ime=chr[i].text[11:]
+    else:
+        items[i].ime = chr[i].text
+    items[i].cijenaString = cijeneFinal[i].text
     #turning the string we got from the price in the $3.45 format into a 3.45 float
-    if(any(char.isdigit() for char in str(cijene[i].string))):
+    if(any(char.isdigit() for char in str(cijeneFinal[i].string))):
         #TODO fix the big numbers being a problem by deleting all '.' characters and replacing all ',' characters with '.'
-        if(',' in str(cijene[i].string)[1:]):
-            replaced = str(cijene[i].string)[1:].replace(".","")
+        if(',' in str(cijeneFinal[i].string)[1:]):
+            replaced = str(cijeneFinal[i].string)[1:].replace(".","")
             replaced = replaced[:-2]
-            print("REPLACED: "+ replaced)
             replaced = replaced.replace(",",".")
-            print("REPLACED2: "+ replaced)
             items[i].cijena = float(replaced)
         else:
-            items[i].cijena = float(str(cijene[i].string)[1:])
+            items[i].cijena = float(str(cijeneFinal[i].string)[1:])
     #if the price isn't a price at all we set the value to the first decimal of the first number ($0.13 to $13.5) => 0.1
     else:
-        items[i].cijena = float(str(cijene[i].text)[1:4])
-        #print("|||"+str(float(str(cijene[i].text)[1:4])))
+        items[i].cijena = float(str(cijeneFinal[i].text)[1:4])
+        #print("|||"+str(float(str(cijeneFinal[i].text)[1:4])))
     #we assign the href memeber of the tag to the link variable of object
     items[i].link = links[i]['href']
 
 #sorting the array of objects by attribute cijena
-items.sort(key=lambda x: x.cijena, reverse=False)
+#items.sort(key=lambda x: x.cijena, reverse=False)
 #prints list of items until upper limit
 for x in items:
     if(x.cijena != None and x.cijena<filterCijena and x.cijena != -0.5):
