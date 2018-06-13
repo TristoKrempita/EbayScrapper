9db0a3135d5c055427d96a014fb57c03b72e853c
Tristo <15713784+TristoKrempita@users.noreply.github.com>
2018-06-13 09:27:22 +0200

    Fixed some stuff, messed up others
    Fixed the big number problem
    Fixed the sorting of "$ to $" format //still have to put it to search for a whitespace and then stop because the numbers keep getting higher and it can only do small ones

--- boi.py
+++ boi.py
@@ -13,37 +13,47 @@ soup = BeautifulSoup(r_html,"html.parser")
 chr = soup.find_all('h3',class_="s-item__title")
 cijene = soup.find_all('span',class_="s-item__price")
 links = soup.find_all('a',class_="s-item__link")
+values = [len(chr),len(cijene),len(links)]
+print(values)
+print(cijene[:2])
+print(chr[:2])
 br=0
 #for c in cijene:
 #    print(str(c.string)[1:])
 
 filterCijena = float(input("Upper limit filter price: "))
-
-for cijen in cijene:
-    print(cijen.text)
-
+print(">"+str(filterCijena))
 class item:
     ime = None
     cijena = None
     cijenaString = None
     link = None
 
+replaced = ""
 items = []
 itemsFinal=[]
-for graf in range(len(cijene)):
+for graf in range(min(values)):
     items.append(item())
-for i in range(0,len(cijene)):
+for i in range(0,len(chr)):
     #assigns title text to the ime variable of object
     items[i].ime = chr[i].text
     items[i].cijenaString = cijene[i].text
     #turning the string we got from the price in the $3.45 format into a 3.45 float
     if(any(char.isdigit() for char in str(cijene[i].string))):
-        items[i].cijena = float(str(cijene[i].string)[1:])
-    #if the price isn't a price at all we set the value to -0.5 and filter it later
+        #TODO fix the big numbers being a problem by deleting all '.' characters and replacing all ',' characters with '.'
+        if(',' in str(cijene[i].string)[1:]):
+            replaced = str(cijene[i].string)[1:].replace(".","")
+            replaced = replaced[:-2]
+            print("REPLACED: "+ replaced)
+            replaced = replaced.replace(",",".")
+            print("REPLACED2: "+ replaced)
+            items[i].cijena = float(replaced)
+        else:
+            items[i].cijena = float(str(cijene[i].string)[1:])
+    #if the price isn't a price at all we set the value to the first decimal of the first number ($0.13 to $13.5) => 0.1
     else:
-        #TODO skuzit kako uzet prvu znameknu tj prvi clan stringa i pretvorit u float (to je za sortiranje ali display ce bit cijenaString)
-        print("HAH",float(str(cijene[i].text)[1]))
-        items[i].cijena = float(str(cijene[i].text)[1])
+        items[i].cijena = float(str(cijene[i].text)[1:4])
+        #print("|||"+str(float(str(cijene[i].text)[1:4])))
     #we assign the href memeber of the tag to the link variable of object
     items[i].link = links[i]['href']
 
